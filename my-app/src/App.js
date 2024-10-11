8ebacf09794f444b7e7467e4a3767f6c3fc58841
Ana Clara Castro <103144032+aclaracastro@users.noreply.github.com>
2024-10-10 22:22:03 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const App = () => {
     return (
         <div>
-            {/* insira qualquer componente da pasta componentes aqui para testar a aplicacao */}
+            {/* insira qualquer componente da pasta componentes aqui para testar a aplicacao */} //oi 
         </div>
     );
 };
