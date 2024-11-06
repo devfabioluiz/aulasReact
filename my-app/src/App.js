0d96f0b8b613f902fe2e89bc0a13ed3ca2a81d18
Fabio Luiz Daemon Da Silva <TR044710@comgas.com.br>
2024-11-05 21:31:53 -0300

    aula 9 - parte 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,13 @@
 // App.js
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  Link,
-} from "react-router-dom";
-import Home from "./components/Aula 8/Home";
-import Users from "./components/Aula 8/Users";
-import UserDetail from "./components/Aula 8/UserDetail";
 import StyledButton from "./components/Aula 9/StyledButton";
 import StyledCard from "./components/Aula 9/StyledCard";
+import ExampleMockApi from "./components/Aula 9/ExampleMockApi";
 
 const App = () => {
   return (
     <>
-      <StyledButton />
-      <StyledCard />
+      <ExampleMockApi />
     </>
   );
 };
