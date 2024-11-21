4537702469fca6d35590ff4ead3e8b97a41c9724
MarioPdr <mario.coctop@gmail.com>
2024-11-21 20:50:44 -0300

    Cards

--- trabalho-final/src/components/Cards/Cards.js
+++ trabalho-final/src/components/Cards/Cards.js
@@ -1,35 +1,22 @@
 import React from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
-const conjuntoUniforme = [
-    {
-        id: 1 ,
-        cor: "azul" ,
-        descricao: "" ,
-        url: "https://raw.githubusercontent.com/xfiveco/mock-api-images/main/images/img-01-xs.jpg",
-    },
-    {
-        id: 2,
-        cor: "branco" ,
-        descricao: "" ,
-        url: "https://raw.githubusercontent.com/xfiveco/mock-api-images/main/images/img-01-xs.jpg",
-      },
-      {
-        id: 3,
-        cor: "preto" ,
-        descricao: "" ,
-        url: "https://raw.githubusercontent.com/xfiveco/mock-api-images/main/images/img-01-xs.jpg",
-      },
+const Cards = ({ image, description }) => {
+  return (
+    <Card sx={{ maxWidth: 345, margin: "1rem auto", boxShadow: 3 }}>
+      <CardMedia
+        component="img"
+        height="200"
+        image={image}
+        alt="Card Image"
+      />
+      <CardContent>
+        <Typography variant="body1" color="text.primary">
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
-];
-
-const Card = () => {
-    return (
-      <>
-        {conjuntoUniforme.map((uniforme) => (
-          <img src={uniforme.url} />
-        ))}
-      </>
-    );
-  };
-
-  export default Card;
+export default Cards;
